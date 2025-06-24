bdc3d962f6a4253d264a927774b73b1d1866df68
lee <desfigne@gmail.com>
2025-06-24 16:50:45 +0900

    change md

---
+++ src/chapter02/Hello.java
@@ -0,0 +1,9 @@
+package chapter02;
+
+public class Hello {
+	//main method
+	public static void main(String[] args) {
+		System.out.print("welcome to JAVA!!");
+		System.out.println("hello~");
+	}
+}
--- src/chapter02/HelloJava.java
+++ src/chapter02/HelloJava.java
@@ -1,6 +1,6 @@
 package chapter02;
 
-class HelloJava {
+public class HelloJava {
 
 	//main 메소드 - 실행을 위한 필수 메소드
 	public static void main(String[] args) {
