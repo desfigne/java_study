867502a78989471c1951454d18e048cef6aa6f26
lee <desfigne@gmail.com>
2025-06-30 18:10:56 +0900

    add data

--- src/chapter02/A021Hello.java
+++ src/chapter02/A021Hello.java
@@ -1,10 +1,11 @@
 package chapter02;
 
-public class HelloJava {
-
+public class A021Hello {
+	
 	//main 메소드 - 실행을 위한 필수 메소드
 	public static void main(String[] args) {
-		System.out.println("welcome to JavaWorld!!!");
+		System.out.print("welcome to JAVA!!");
+		System.out.println("hello~");
 	}
-
 }
+ 
