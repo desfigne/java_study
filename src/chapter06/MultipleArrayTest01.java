70feac46002f8fc01349731db04e3eb0e41e7c20
lee <desfigne@gmail.com>
2025-06-30 17:47:27 +0900

    add data

--- src/chapter06/MultipleArrayTest01.java
+++ src/chapter06/MultipleArrayTest01.java
@@ -35,7 +35,7 @@ public class MultipleArrayTest01 {
 		System.out.println();
 		for(int i=0; i<matrix2.length; i++) {
 			for(int j=0; j<matrix2[i].length; j++) {
-				System.out.print(" [" + i + ", " + j + "] = " + matrix[i][j] + "\t\t");
+				System.out.print(" [" + i + ", " + j + "] = " + matrix2[i][j] + "\t\t");
 			}
 			
 			System.out.println();
@@ -48,7 +48,7 @@ public class MultipleArrayTest01 {
 		System.out.println();
 		for(int i=0; i<matrix3.length; i++) {
 			for(int j=0; j<matrix3[i].length; j++) {
-				System.out.print(" [" + i + ", " + j + "] = " + matrix[i][j] + "\t\t");
+				System.out.print(" [" + i + ", " + j + "] = " + matrix3[i][j] + "\t\t");
 			}
 			
 			System.out.println();
