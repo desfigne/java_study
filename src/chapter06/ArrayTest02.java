651f38700392259bb80d70a7b37a97cd1bd77334
lee <desfigne@gmail.com>
2025-06-30 10:55:14 +0900

    add data

---
+++ src/chapter06/ArrayTest02.java
@@ -0,0 +1,31 @@
+package chapter06;
+
+public class ArrayTest02 {
+
+	public static void main(String[] args) {
+		
+//		// names 문자열 변수 선언 및 생성
+//		String[] names;
+//		String names2[];
+//		
+//		names = new String[3];
+//		names2 = new String[5];
+//		
+//		int[] scores = new int[10];
+//		int scores2[] = new int[3];
+//		int scores3[] = { 10, 20, 30 };
+//		String[] names3 = { "홍길동" , "홍길순" };
+		
+		String names[];
+		String[] names2;
+		
+		names = new String[2];
+		names2 = new String[5];
+		
+		int[] scores = new int[3];
+		
+		System.out.println(scores);
+		
+	}
+
+}
