c00d7f8744d61a6af6cf6d861e1aa95be0711e16
lee <desfigne@gmail.com>
2025-06-30 22:27:27 +0900

    change data

---
+++ src/chapter04/JAVA0402ConditionalOperator.java
@@ -0,0 +1,26 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class JAVA0402ConditionalOperator {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요.");
+		
+		int age = 19;
+		boolean isMember= true;
+		
+		String result = (age>=20 && isMember) ? "성인 회원" : "조건 미달";
+		System.out.println(result);
+		
+		int score = 55;
+		boolean isRetake = true;
+		
+		String result2 = (score >= 60 || isRetake) ? "통과" : "탈락";
+		System.out.println(result2);
+		
+		
+	}
+
+}
