44aa7dd2af3fa5bb07fc5c5f9e70a40d427cbc7a
lee <desfigne@gmail.com>
2025-07-15 17:46:03 +0900

    changed data

---
+++ src/chapter10/JAVA1001Outer02.java
@@ -0,0 +1,17 @@
+package chapter10;
+
+public class JAVA1001Outer02 {
+
+	public static void main(String[] args) {
+
+		JAVA1001Outer01 outer = new JAVA1001Outer01();
+		System.out.println("outer.name --> " + outer.name);
+		
+//		Inner inner = new JAVA1001Outer.Inner(); // 아우터가 없으면 호출이 안됨
+		JAVA1001Outer01.Inner inner = new JAVA1001Outer01.Inner(); // static이 없어서 메모리에 올라가지 않아 불러올 수 없음
+		System.out.println("outer.name --> " + inner.name);
+//		System.out.println("outer.inner.name --> " + outer.inner.name); // 이너 안의 스트링에 스태틱이 없어 호출 안됨 --> 강사님이랑 학생들은 되는데 나는 왜 안되는지 확인하기
+
+	}
+
+}
